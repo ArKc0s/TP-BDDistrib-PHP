d07e6eebf4ca1c054695ed6fd70ab7eb82b3c238
Aurélien Houdart <aure.houdart@gmail.com>
2024-01-11 18:17:15 +0100

    modif commande

--- app/Views/commande_membre.php
+++ app/Views/commande_membre.php
@@ -10,40 +10,7 @@
 <body>
     <h2>Formulaire de Commande</h2>
     <form action="traitement_commande.php" method="post">
-        <?php
-        // Connexion à la base de données MongoDB (à adapter selon votre configuration)
-        $client = new MongoDB\Client("mongodb://localhost:27017");
-        $db = $client->selectDatabase("votre_base_de_donnees");
-
-        // Récupérer la liste des membres pour la liste déroulante
-        $membresCollection = $db->selectCollection("membres");
-        $membresCursor = $membresCollection->find([], ['projection' => ['nom' => 1]]);
-        
-        echo '<label for="id_membre">Sélectionnez le Membre:</label>';
-        echo '<select name="id_membre" required>';
-        
-        // Afficher chaque membre comme une option dans la liste déroulante
-        foreach ($membresCursor as $membre) {
-            echo '<option value="' . $membre["_id"] . '">' . $membre["nom"] . '</option>';
-        }
-
-        echo '</select><br>';
-        
-        // Récupérer les noms de matériel depuis la base de données MongoDB
-        $materielsCollection = $db->selectCollection("materiels");
-        $materielsCursor = $materielsCollection->find([], ['projection' => ['nom_materiel' => 1, 'prix' => 1]]);
-        
-        echo '<label for="materiaux">Choisissez un Matériel:</label>';
-        echo '<select name="materiaux[]" multiple required>';
-        
-        // Afficher chaque matériel comme une option dans la liste déroulante
-        foreach ($materielsCursor as $materiel) {
-            echo '<option value="' . $materiel["_id"] . '">' . $materiel["nom_materiel"] . ' - Prix: ' . $materiel["prix"] . '</option>';
-        }
-
-        echo '</select><br>';
-        ?>
-        
+               
         <label for="date_commande">Date de Commande:</label>
         <input type="date" name="date_commande" required><br>
 
