a9e0378c44076f766692077860e9797ae1132c2b
Léo Wadin <46723431+ArKc0s@users.noreply.github.com>
2024-02-12 22:52:24 +0100

    feat: Création d'un groupe (front + back)

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,6 +13,10 @@ $routes->get('/groupe/creer', 'GroupeController::creer',['filter' => 'authGuard'
 $routes->get('/materiel/ajouter', 'MaterielController::ajouter',['filter' => 'authGuard']);
 $routes->get('/commande', 'CommandeController::commander',['filter' => 'authGuard']);
 $routes->get('/historique', 'HistoriqueController::search',['filter' => 'authGuard']);
+
+
 $routes->match(['get', 'post'], '/store', 'MembreController::store');
 $routes->match(['get', 'post'], '/loginAuth', 'MembreController::loginAuth');
+
+$routes->post('/api/groupe/creation', 'GroupeController::creationAPI',['filter' => 'authGuard']);
 
--- app/Controllers/GroupeController.php
+++ app/Controllers/GroupeController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\Groupe;
+use App\Models\Ville;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class GroupeController extends BaseController
@@ -14,6 +16,19 @@ class GroupeController extends BaseController
 
     public function creer()
     {
-        return view('creer_groupe');
+        $groupe = new Groupe();
+        $ville = new Ville();
+        $data['next_num'] = sprintf("%03d", intval($groupe->getLastNum()) + 1);
+        $data["villes"] = $ville->getList();
+        return view('creer_groupe', $data);
+    }
+
+    public function creationAPI()
+    {
+        $data = $this->request->getPost();
+        $groupe = new Groupe();
+        $groupe->createGroupe($data["num_groupe"], $data["nom_groupe"], $data["ville"]);
+
+        return redirect()->back();
     }
 }
